a4588278089ffe6e28cc586ea60f3072dc4aeb8c
Bruno Felix <brunofelixdesousa@hotmail.com>
2023-04-27 02:31:58 -0300

    *** modulo de venda em construção ***

--- controllers/VendaController.js
+++ controllers/VendaController.js
@@ -15,41 +15,98 @@ module.exports = class VendaController {
 
     static async mostrarFormularioVenda(req, res) {
         try {
-            // const produtos = await Produto.findAll({raw: true})
-            // const clientes = await Cliente.findAll({raw: true})
-            // console.log(produtos, clientes)
-            res.render('venda/venda')
+
+            const produtos = await Produto.findAll({ raw: true })
+
+            res.render('venda/venda', { produtos })
         } catch (err) {
             console.error(err)
             res.status(500).send('Erro ao buscar produtos e clientes')
         }
     }
 
-    static async listaCompra(req, res) {
+    // static async buscarProdutos(req, res) {
+    //     try {
+    //       const termo = req.query.search
+    
+    //       const produtos = await Produto.findAll({
+    //         where: {
+    //           nome: {
+    //             [Op.like]: `%${termo}%`
+    //           }
+    //         },
+    //         raw: true
+    //       })
+    
+    //       res.render('venda/produtos', { produtos })
+    //     } catch (err) {
+    //       console.error(err)
+    //       res.status(500).send('Erro ao buscar produtos')
+    //     }
+    //   }
 
-        let search = ''
-
-        if (req.query.search) {
-            search = req.query.search
-        }
-
-        console.log("==============>" + search)
-
+      static async listaCompra(req, res) {
         try {
-            const produto = await Produto.findAll({
-                raw: true,
-                where: {
-                    nome: {[ Op.like ]: `%${search}%`}
-                }
-            })
-            adicionarProduto(produto)
-            console.log("==============>" + produto)
-            console.log("==============>" + carrinho)
+            const termo = req.query.search
+    
+                  const produtodata = await Produto.findAll({
+                    where: {
+                      nome: {
+                        [Op.like]: `%${termo}%`
+                      }
+                    },
+                  })
+
+                  const produtos = produtodata.map((result) => result.get({plain: true}))
+    
+        //   adicionarProduto(produtos)
+    console.log("==================> " + produtodata.nome)
+        //   const total = carrinho.reduce((acc, produto) => {
+        //     return acc + produto.preco
+        //   }, 0)
+    
+          res.render('venda/venda', { carrinho })
         } catch (err) {
-            console.log(err)
-            req.flash('message', '*** ❌ Error ao buscar produto! ***')
-            res.render('venda')
-        }        
-    }
+          console.error(err)
+          res.status(500).send('Erro ao adicionar produto ao carrinho')
+        }
+      }
+
+    // static async buscarProdutos(req, res) {
+    //     try {
+    //         const termoBusca = req.query.search;
+    //         const produtos = await Produto.findAll({
+    //             where: {
+    //                 nome: {[ Op.like ]: `%${termoBusca}%`}
+    //             }
+    //         });
+    //         res.json(produtos);
+    //     } catch (err) {
+    //         console.error(err);
+    //         res.status(500).send('Erro ao buscar produtos');
+    //     }
+    // }
+    
+
+    // static async listaCompra(req, res) {
+    //     const search = req.query.search
+
+    //     console.log("==============>" + search)
+
+    //     try {
+    //         const produto = await Produto.findAll({
+    //             raw: true,
+    //             where: {
+    //                 nome: {[ Op.like ]: `%${search}%`}
+    //             }
+    //         })
+    //         adicionarProduto(produto)
+    //         return
+    //     } catch (err) {
+    //         console.log(err)
+    //         req.flash('message', '*** ❌ Error ao buscar produto! ***')
+    //         res.render('venda')
+    //     }        
+    // }
 
 }
--- routes/VendaRoutes.js
+++ routes/VendaRoutes.js
@@ -6,4 +6,7 @@ const autorizacao = require('../helpers/autorizacao').checkAutorizacao
 
 router.get('/venda', autorizacao, VendaController.mostrarFormularioVenda)
 router.get('/venda/adicionar-produto', autorizacao, VendaController.listaCompra)
+// router.get('/venda/buscar-produtos', autorizacao, VendaController.buscarProdutos)
+
+
 module.exports = router
